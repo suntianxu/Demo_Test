091cc12088bc8ba4c5560a0fe87b37fdccdca5e3
lxy <809086045@qq.com>
2022-01-28 14:35:34 +0800

    修复bug

---
+++ PyDemo/testcase/contract/test_ContractCreateByCategory.py
@@ -0,0 +1,32 @@
+# -*- coding:utf-8 -*-
+# @Time    : 2021/7/14
+# @Author  : Xuanyu Liu
+# @File    : test_ContractCreateByCategory.py
+# ****************************
+import os
+import allure
+import pytest
+from comm.utils.readYaml import read_yaml_data
+from comm.unit.initializePremise import init_premise
+from comm.unit.apiSend import send_request
+from comm.unit.checkResult import check_result
+case_yaml = os.path.realpath(__file__).replace('testcase', 'page').replace('.py', '.yaml')
+case_path = os.path.dirname(case_yaml)
+case_dict = read_yaml_data(case_yaml)
+
+
+# @pytest.mark.skip
+@allure.feature(case_dict["test_info"]["title"])
+class TestContract_Create:
+
+    @allure.title("{case_data[title]}")
+    @pytest.mark.parametrize("case_data", case_dict["test_case"])
+    @allure.story("test_ContractCreateByCategory")
+    def test_ContractCreateByCategory(self, case_data):
+        allure.dynamic.description(case_data['describe'])
+        # 初始化请求：执行前置接口+替换关联变量
+        test_info, case_data = init_premise(case_dict["test_info"], case_data, case_path)
+        # 发送当前接口
+        code, data = send_request(test_info, case_data)
+        # 校验接口返回
+        check_result(case_data, code, data)
